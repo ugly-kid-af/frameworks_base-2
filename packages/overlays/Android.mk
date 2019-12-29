8a5aaf6f53bf49590dffc740af16afbad6740adb
Pranav Vashi <neobuddy89@gmail.com>
2019-12-29 22:39:19 +0530

    Add Comic Sans font styles [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -38,6 +38,7 @@ LOCAL_REQUIRED_MODULES := \
 	FontAmaranteSourceOverlay \
 	FontBariolSourceOverlay \
 	FontCagliostroSourceOverlay \
+	FontComicSansSourceOverlay \
 	FontCoolstorySourceOverlay \
 	FontLGSmartGothicSourceOverlay \
 	FontRosemarySourceOverlay \
