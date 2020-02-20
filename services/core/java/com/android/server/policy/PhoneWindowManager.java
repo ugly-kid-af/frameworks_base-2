0b21c4e14f2deb64c3d041fef0070e3ae27ba44d
KostyaJRZ <54120489+KostyaJRZ@users.noreply.github.com>
2020-02-20 15:17:13 +0200

    Remove duplicate library

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -227,7 +227,6 @@ import com.android.server.wm.AppTransition;
 import com.android.server.wm.DisplayPolicy;
 import com.android.server.wm.DisplayRotation;
 import com.android.server.wm.WindowManagerInternal;
-import com.android.server.policy.pocket.PocketLock;
 import android.pocket.PocketManager;
 import android.pocket.IPocketCallback;
 import com.android.server.wm.WindowManagerInternal.AppTransitionListener;
